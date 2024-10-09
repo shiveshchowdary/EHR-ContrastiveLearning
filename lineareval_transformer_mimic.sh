c019a263c12009a6f668303dacb1c15cdab94750
Shivesh Kodali <shivesh_2001@cse-stmi-s2.engr.tamu.edu>
2024-10-09 09:24:41 -0500

    made final changes

--- lineareval_transformer_mimic.sh
+++ lineareval_transformer_mimic.sh
@@ -1,25 +1,25 @@
 # #!/bin/bash
 
 
-mkdir -p outputs/linear_eval_synthetic_both
+mkdir -p outputs/linear_eval
 
     
-CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 42 > outputs/linear_eval_synthetic_both/phenotyping_linear_eval_seed_42_epoch_24.txt &
+CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 42 > outputs/linear_eval/phenotyping_linear_eval_seed_42_epoch_24.txt &
     
-CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 50 > outputs/linear_eval_synthetic_both/phenotyping_linear_eval_seed_50_epoch_24.txt &
+CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 50 > outputs/linear_eval/phenotyping_linear_eval_seed_50_epoch_24.txt &
     
-CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 60 > outputs/linear_eval_synthetic_both/phenotyping_linear_eval_seed_60_epoch_24.txt &
+CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 60 > outputs/linear_eval/phenotyping_linear_eval_seed_60_epoch_24.txt &
     
-CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 70 > outputs/linear_eval_synthetic_both/phenotyping_linear_eval_seed_70_epoch_24.txt &
+CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 70 > outputs/linear_eval/phenotyping_linear_eval_seed_70_epoch_24.txt &
     
-CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 80 > outputs/linear_eval_synthetic_both/phenotyping_linear_eval_seed_80_epoch_24.txt &
+CUDA_VISIBLE_DEVICES=6 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/phenotyping --K 1 --epochs 20 --head_lr 0.03 --dataset phenotype --patience 20 --batch_size 16 --linear_eval --seed 80 > outputs/linear_eval/phenotyping_linear_eval_seed_80_epoch_24.txt &
 
-CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 42 > outputs/linear_eval_synthetic_both/ihm_linear_eval_seed_42_epoch_24.txt &
+CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 42 > outputs/linear_eval/ihm_linear_eval_seed_42_epoch_24.txt &
 
-CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 50 > outputs/linear_eval_synthetic_both/ihm_linear_eval_seed_50_epoch_24.txt &
+CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 50 > outputs/linear_eval/ihm_linear_eval_seed_50_epoch_24.txt &
 
-CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 60 > outputs/linear_eval_synthetic_both/ihm_linear_eval_seed_60_epoch_24.txt &
+CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 60 > outputs/linear_eval/ihm_linear_eval_seed_60_epoch_24.txt &
 
-CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 70 > outputs/linear_eval_synthetic_both/ihm_linear_eval_seed_70_epoch_24.txt &
+CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 70 > outputs/linear_eval/ihm_linear_eval_seed_70_epoch_24.txt &
 
-CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 80 > outputs/linear_eval_synthetic_both/ihm_linear_eval_seed_80_epoch_24.txt
+CUDA_VISIBLE_DEVICES=5 python main_downstream.py --method Combined --load_epoch 399 --data_dir /ssd-shared/MIMIC_EICU_DATA/data_17/in-hospital-mortality --K 1 --epochs 20 --head_lr 0.01 --dataset ihm --patience 20 --batch_size 16 --linear_eval --seed 80 > outputs/linear_eval/ihm_linear_eval_seed_80_epoch_24.txt
